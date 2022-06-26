22dd8b3b0d0f3cd7fdfbba5677fa8280dcda5023
ju-ei8ht <qkr8125@gmail.com>
2022-06-26 16:28:29 +0900

    tag 수정(posts에서 all tag defalut)

--- public/js/tag.js
+++ public/js/tag.js
@@ -1,9 +1,17 @@
 $(function(){
     let currentTag = "";
+    let url = location.pathname;
+    let urlSplit = url.split('/');
+    let urlLen = urlSplit.length;
+    let queryParent = urlSplit[urlLen-2];
     const queryTag = getQuery().tag;
+    console.log(queryTag);
     if(queryTag){
         currentTag = queryTag;
         filterByName(currentTag)
+    }
+    if(queryTag == undefined && queryParent == "posts"){
+        filterByName("all");
     }
     function getQuery(){
         let query = {};
